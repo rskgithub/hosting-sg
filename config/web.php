71bb21df69850002ad4176fb2626ec1c390e6ff1
Roman Nordway <nordway88@gmail.com>
2015-06-01 21:09:12 +0300

    Create project

---
+++ config/web.php
@@ -0,0 +1,49 @@
+<?php
+
+$config = [
+    'id' => 'basic',
+	'name' => 'Hosting Control Panel',
+	'language' => 'ru-RU',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'components' => [
+        'request' => [
+            'cookieValidationKey' => 'TjNadAD3okPlGle85Mn6457IBW2NvT2r',
+        ],
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'user' => [
+            'identityClass' => 'app\models\User',
+            'enableAutoLogin' => true,
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'useFileTransport' => false,
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'db' => require(__DIR__ . '/db.php'),
+    ],
+    'params' => require(__DIR__ . '/params.php'),
+];
+
+if (YII_ENV_DEV) {
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = 'yii\debug\Module';
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = 'yii\gii\Module';
+}
+
+return $config;
